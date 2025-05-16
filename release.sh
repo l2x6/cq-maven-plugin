eceb718ffac1696477af235a071995ddde5e707f
Peter Palaga <ppalaga@redhat.com>
2025-05-16 13:12:39 +0200

    Use micro++ as a next development version when releasing

--- release.sh
+++ release.sh
@@ -18,18 +18,33 @@
 
 # A script for editting .github/project.yml file, committing the changes and pushing the brach to upstream
 
-set -x
 set -e
+upstreamUrl="git@github.com:l2x6/cq-maven-plugin.git"
 
-releaseVersion="$1"
-nextVersion="$2"
+if [ "$#" -eq  "2" ]; then
+    releaseVersion="$1"
+    array=($(echo "$releaseVersion" | tr . '\n'))
+    releaseVersionMajorMinor="$((array[0])).$((array[1]))"
+    nextVersion="$2"
+elif [ "$#" -eq  "1" ]; then
+    releaseVersion="$1"
+    array=($(echo "$releaseVersion" | tr . '\n'))
+    releaseVersionMajorMinor="$((array[0])).$((array[1]))"
+    array[2]=$((array[2]+1))
+    nextVersion="$(IFS=. ; echo "${array[*]}")-SNAPSHOT"
+    echo "Setting default nextVersion ${nextVersion}"
+else
+    echo "One or two params expected: $0 <release-version>[ <next-development-version>]"
+fi
 
+set -x
 topicBranch=trigger-release-$releaseVersion
 git checkout -b $topicBranch
 
 sed -i -e 's|  current-version:.*|  current-version: '$releaseVersion'|' .github/project.yml
 sed -i -e 's|  next-version:.*|  next-version: '$nextVersion'|' .github/project.yml
+sed -i -e 's|  current-major-minor-version:.*|  current-major-minor-version: '$releaseVersionMajorMinor'|' .github/project.yml
 
 git add -A
 git commit -m "Trigger release $releaseVersion"
-git push upstream $topicBranch
+git push "${upstreamUrl}" $topicBranch
